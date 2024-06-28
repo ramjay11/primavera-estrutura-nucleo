649e185753943aa4b685293ccf36b23056d555b0
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2024-06-29 00:23:07 +0800

    Compromisso inicial

---
+++ mola-inversao-de-controlo/src/main/java/com/ramjava/mola/inversao/de/controlo/MolaInversaoDeControloApplication.java
@@ -0,0 +1,13 @@
+package com.ramjava.mola.inversao.de.controlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolaInversaoDeControloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolaInversaoDeControloApplication.class, args);
+	}
+
+}
---
+++ mola-inversao-de-controlo/src/test/java/com/ramjava/mola/inversao/de/controlo/MolaInversaoDeControloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramjava.mola.inversao.de.controlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolaInversaoDeControloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
